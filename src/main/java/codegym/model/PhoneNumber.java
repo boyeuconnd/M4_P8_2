0e26e60c43f47d9f622e3d2340bb12e390c36f96
Dang Pham <thienvu453@gmail.com>
2020-06-25 10:31:32 +0700

    Add project and first commit

---
+++ src/main/java/codegym/model/PhoneNumber.java
@@ -0,0 +1,67 @@
+package codegym.model;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import javax.validation.Validation;
+
+@Component
+public class PhoneNumber implements Validator {
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    /**
+     * Can this {@link Validator} {@link #validate(Object, Errors) validate}
+     * instances of the supplied {@code clazz}?
+     * <p>This method is <i>typically</i> implemented like so:
+     * <pre class="code">return Foo.class.isAssignableFrom(clazz);</pre>
+     * (Where {@code Foo} is the class (or superclass) of the actual
+     * object instance that is to be {@link #validate(Object, Errors) validated}.)
+     *
+     * @param clazz the {@link Class} that this {@link Validator} is
+     *              being asked if it can {@link #validate(Object, Errors) validate}
+     * @return {@code true} if this {@link Validator} can indeed
+     * {@link #validate(Object, Errors) validate} instances of the
+     * supplied {@code clazz}
+     */
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return PhoneNumber.class.isAssignableFrom(clazz);
+    }
+
+    /**
+     * Validate the supplied {@code target} object, which must be
+     * of a {@link Class} for which the {@link #supports(Class)} method
+     * typically has (or would) return {@code true}.
+     * <p>The supplied {@link Errors errors} instance can be used to report
+     * any resulting validation errors.
+     *
+     * @param target the object that is to be validated (can be {@code null})
+     * @param errors contextual state about the validation process (never {@code null})
+     * @see ValidationUtils
+     */
+    @Override
+    public void validate(Object target, Errors errors) {
+        PhoneNumber phoneNumber = (PhoneNumber) target;
+        String number = phoneNumber.getNumber();
+        ValidationUtils.rejectIfEmpty(errors, "number", "number.empty");
+        if (number.length()>11 || number.length()<10){
+            errors.rejectValue("number", "number.length");
+        }
+        if (!number.startsWith("0")){
+            errors.rejectValue("number", "number.startsWith");
+        }
+        if (!number.matches("(^$|[0-9]*$)")){
+            errors.rejectValue("number", "number.matches");
+        }
+    }
+}
